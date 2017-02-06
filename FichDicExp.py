2eee0f20e5d5c06d91af031bf42c9e98aaf81e33
ChristianBermejo <cbermejog7gmail.com>
2017-02-06 17:13:43 +0100

    Utilizando dicc y excepciones

--- FichDicExp.py
+++ FichDicExp.py
@@ -1,15 +1,15 @@
 #!usr/bin/python3
-try:
-    fich=open("/etc/passwd","r")
-    usuarios=fich.readlines()
-    fich.close()
-    dicc = {}
-    root = 'root'
-    imaginario = 'imaginario'
-    for usuario in usuarios:
-        dicc[usuario.split(':')[0]] = (usuario.split(':')[-1])
+fich = open("/etc/passwd","r")
+usuarios = fich.readlines()
+fich.close()
+dicc = {}
+root = 'root'
+imaginario = 'imaginario'
+for usuario in usuarios:
+    dicc[usuario.split(':')[0]] = (usuario.split(':')[-1][:-1])
 
+try:
     print("Key:", root, "---> Value:", dicc[root])
-    print(dicc[imaginario])
-except KeyError: 
+    print("Key:", imaginario,"---> Value:", dicc[imaginario])
+except KeyError:
     print("No existe esa key en este diccionario")
